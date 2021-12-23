553c2959b9f7668a4dad9dff307476436f12c89d
Lucas Nery <lucasnerytoledo@hotmail.com>
2021-12-23 20:10:34 -0300

    Atualizando calculo de frete

--- src/components/CalcularFrete.vue
+++ src/components/CalcularFrete.vue
@@ -64,8 +64,6 @@
 
 <script>
 
-import {calcularPrecoPrazo} from 'correios-brasil';
-
 export default {
 
     props: ['valorProdutos'],
@@ -86,20 +84,6 @@ export default {
         calcularFrete() {
             this.valorFrete = 46,99
             this.mensagemErro = null
-            let args = {
-                sCepOrigem: '81200100',
-                sCepDestino: '21770200',
-                nVlPeso: '1',
-                nCdFormato: '1',
-                nVlComprimento: '20',
-                nVlAltura: '20',
-                nVlLargura: '20',
-                nCdServico: ['04014', '04510'], //Array com os códigos de serviço
-                nVlDiametro: '0',
-            }
-            calcularPrecoPrazo(args).then((response) => {
-                console.log(response);
-            });
         },
 
         calcularCupom(text_cupom) {
